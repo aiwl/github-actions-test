66b0d1f591234d8224611b117ea0be261aecea7d
Arno Luebke <aiwluebke@gmail.com>
2021-09-27 14:45:34 +0900

    test

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+#include "foo/foo.hpp"
+
+int
+main(int argc, char **argv)
+{
+    int f = foo();
+    return 0; 
+}
---
+++ tests/test_foo.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <foo/foo.hpp>
+
+#define ASSERT(expr) do { if (!expr) { std::fprintf(stderr, "Test failed\n"); exit(1); } } while(0)
+
+static void
+test_foo()
+{
+    ASSERT(42 == foo());
+}
+
+int
+main(int argc, char **argv)
+{
+    test_foo();
+    return 0;
+}
